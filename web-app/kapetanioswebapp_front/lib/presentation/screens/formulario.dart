555a44cc21150a042073f8dc745eb8ad386fb041
luis.hermosillo <luis.hermosillo@coppel.com>
2025-01-06 20:56:53 -0700

    chore: Avance en ventana de registro

--- web-app/kapetanioswebapp_front/lib/presentation/screens/formulario.dart
+++ web-app/kapetanioswebapp_front/lib/presentation/screens/formulario.dart
@@ -16,11 +16,12 @@ class _RegistroNuevoMonitorState extends State<RegistroNuevoMonitor> {
     fontWeight: FontWeight.w500
   );
   String selectedItem = "Operator";
+  String selectedMenuItem = "";
   String instructionsInstallOperator = """
 helm repo add datadog https://helm.datadoghq.com
 helm install datadog-operator datadog/datadog-operator
-kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2
-  """;
+kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2""";
+  String deployText = "kubectl apply -f datadog-agent.yaml";
 
   @override
   Widget build(BuildContext context) {
@@ -28,78 +29,253 @@ kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f
       body: Container(
         height: MediaQuery.sizeOf(context).height,
         width: MediaQuery.sizeOf(context).width,
-        child: SingleChildScrollView(
-          child: Row(
-            children: [
-              Container( 
-                height: MediaQuery.sizeOf(context).height,
-                width: MediaQuery.sizeOf(context).width*.2,
-                color: Colors.red,
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    InkWell(
-                      onTap: (){},
-                      child: Container(
+        child: Row(
+          children: [
+            Container( 
+              height: MediaQuery.sizeOf(context).height,
+              width: MediaQuery.sizeOf(context).width*.2,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  lateralMenuButton("Kubernetes", FontAwesomeIcons.docker)
+                ],
+              ),
+            ),
+            ScrollConfiguration(
+              behavior:  NoScrollBarBehavior(),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Instrucciones de instalacion", style: tituloStilo,),
+                      instructionTitle("Metodo de instalacion", 1),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
                         child: Row(
                           children: [
-                            Icon(FontAwesomeIcons.docker)
+                            methodButton("Operator"),
+                            methodButton("Heml Chart"),
                           ],
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Instrucciones de instalacion", style: tituloStilo,),
-                    instructionTitle("Metodo de instalacion", 1),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 40),
-                      child: Row(
-                        children: [
-                          methodButton("Operator"),
-                          methodButton("Heml Chart"),
-                        ],
-                      ),
-                    ),
-                    instructionTitle("Instalar operador", 2),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 40),
-                      child: SelectionArea(
-                        child: Container(
-                          width: MediaQuery.sizeOf(context).width*.7,
-                          height: 110,
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: Colors.grey.withAlpha(80)
-                          ),
-                          child: Text(
-                            instructionsInstallOperator, 
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
+                      instructionTitle("Instalar operador", 2),
+                      grayContainerText(context, instructionsInstallOperator),
+                      instructionTitle("Configurar yml", 3),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 700,
+                              color: const Color(0xFF2D2D30),
+                              padding: EdgeInsets.all(14),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  lineYML("apiVersion", "datadoghq.com/v2alpha1", 0, 1),
+                                  lineYML("kind", "DatadogAgent", 0, 2),
+                                  lineYML("metadata", "", 0, 3),
+                                  lineYML("name", "datadog", 5, 4),
+                                  lineYML("spec", "", 0, 5),
+                                  lineYML("global", "", 5, 6),
+                                  lineYML("credentials", "", 10, 7),
+                                  lineYML("apiSecret", "", 15, 8),
+                                  lineYML("secretName", "datadog-secret", 20, 9),
+                                  lineYML("keyName", "keyname", 20, 10)
+                                ],
+                              ),
                             ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Container(
+                                width: MediaQuery.sizeOf(context).width*.324,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          width: 1.0,
+                                          color: Colors.grey.withAlpha(100)
+                                        ),
+                                      ),
+                                      child: ExpansionTile(
+                                        title: Text("Global Variables"),
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              onChanged: (value) {
+                                                print("3213");
+                                              },
+                                              decoration: InputDecoration(
+                                                hintText: "Nombre del cluster",
+                                                labelText: "Nombre del cluster",
+                                                border: OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(8),
+                                                  borderSide: BorderSide(color: Colors.black, width: 1),
+                                                ),
+                                              ),
+                                            ),
+                                          ),Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              onChanged: (value) {
+                                                print("3213");
+                                              },
+                                              decoration: InputDecoration(
+                                                hintText: "Registry host",
+                                                labelText: "Registry host",
+                                                border: OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(8),
+                                                  borderSide: BorderSide(color: Colors.black, width: 1),
+                                                ),
+                                              ),
+                                            ),
+                                          ),Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              onChanged: (value) {
+                                                print("3213");
+                                              },
+                                              decoration: InputDecoration(
+                                                hintText: "Kapetanios host",
+                                                labelText: "Kapetanios host",
+                                                border: OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(8),
+                                                  borderSide: BorderSide(color: Colors.black, width: 1),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      instructionTitle("Desplegar agente con la configuracion anterior", 4),
+                      grayContainerText(context, deployText),
+                      instructionTitle("Confirmar instalacion de agente", 5),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Container(
+                          height: MediaQuery.sizeOf(context).height*.3,
+                          width: MediaQuery.sizeOf(context).width*.7,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1,
+                              color: Color.fromRGBO(189, 189, 189, 1)
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8)
+                            )
+                          ),
+                          child: DataTable(
+                            columns: const <DataColumn>[
+                              DataColumn(label: Text("POD")),
+                              DataColumn(label: Text("STATUS")),
+                              DataColumn(label: Text("AGE")),
+                              DataColumn(label: Text("READY")),
+                              DataColumn(label: Text("RESTART"))
+                            ], rows: const <DataRow>[
+                              DataRow(
+                                cells: <DataCell>[
+                                  DataCell(Text("1")),
+                                  DataCell(Text("1")),
+                                  DataCell(Text("1")),
+                                  DataCell(Text("1")),
+                                  DataCell(Text("1")),
+                                ]
+                              )
+                            ]
                           ),
                         ),
                       ),
-                    ),
-                    instructionTitle("Configurar yml", 3),
-                    instructionTitle("Desplegar agente con la configuracion anterior", 4),
-                    instructionTitle("Confirmar instalacion de agente", 5),
-                  ],
+                    ],
+                  ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       )
+    );
+  }
+
+  InkWell lateralMenuButton(String texto, IconData icono) {
+    return InkWell(
+      onTap: (){
+        setState(() {
+          selectedMenuItem = texto;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        alignment: Alignment.center,
+        height: 50,
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0)
+          ),
+          border: Border.all(
+            width: 1,
+            color: selectedMenuItem == texto ? Colors.white : Colors.grey.withAlpha(100)
+          ),
+          color: selectedMenuItem == texto ? Colors.blueAccent : Colors.white,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              icono,
+              color: selectedMenuItem == texto ? Colors.white : Colors.black,
+            ),
+            SizedBox(width: 20,),
+            Text(texto, style: TextStyle(
+              fontSize: 18,
+              color: selectedMenuItem == texto ? Colors.white : Colors.black
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding grayContainerText(BuildContext context, String texto) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 40),
+      child: SelectionArea(
+        child: Container(
+          width: MediaQuery.sizeOf(context).width*.7,
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.grey.withAlpha(30)
+          ),
+          child: Text(
+            texto, 
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -114,14 +290,12 @@ kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f
               instructionsInstallOperator = """
 helm repo add datadog https://helm.datadoghq.com
 helm install datadog-operator datadog/datadog-operator
-kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2
-              """;
+kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2""";
             }else{
               instructionsInstallOperator = """
 helm repo add datadog https://helm.datadoghq.com
 helm repo update
-kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2
-              """;
+kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f51ba73cf9c3bede5088bc2""";
             }
           });
         },
@@ -179,30 +353,7 @@ kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f
   }
 
 
-  // Container(
-  //             height: MediaQuery.sizeOf(context).height,
-  //             width: MediaQuery.sizeOf(context).width * .4,
-  //             color: const Color(0xFF2D2D30),
-  //             padding: EdgeInsets.all(14),
-  //             child: Column(
-  //               mainAxisAlignment: MainAxisAlignment.start,
-  //               crossAxisAlignment: CrossAxisAlignment.start,
-  //               children: [
-  //                 Text("Configuracion de agente", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),),
-  //                 SizedBox(height: 20,),
-  //                 lineYML("apiVersion", "datadoghq.com/v2alpha1", 0, 1),
-  //                 lineYML("kind", "DatadogAgent", 0, 2),
-  //                 lineYML("metadata", "", 0, 3),
-  //                 lineYML("name", "datadog", 5, 4),
-  //                 lineYML("spec", "", 0, 5),
-  //                 lineYML("global", "", 5, 6),
-  //                 lineYML("credentials", "", 10, 7),
-  //                 lineYML("apiSecret", "", 15, 8),
-  //                 lineYML("secretName", "datadog-secret", 20, 9),
-  //                 lineYML("keyName", "keyname", 20, 10)
-  //               ],
-  //             ),
-  //           )
+  
 
   Widget lineYML(String clave, String valor, int tabulador, int i){
     return Padding(
@@ -227,4 +378,16 @@ kubectl create secret generic datadog-secret --from-literal \napi-key=5bb1b4ba6f
   }
 
    
+}
+
+class NoScrollBarBehavior extends ScrollBehavior {
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const ClampingScrollPhysics(); // Deshabilita cualquier efecto de rebote
+  }
+
+  @override
+  Widget buildScrollbar(BuildContext context, Widget child, ScrollableDetails details) {
+    return child; // Elimina la barra de scroll
+  }
 }
